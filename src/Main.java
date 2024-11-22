a08a72d280003e5943570caee6fb4af640b81a82
malika099 <ergasevamadina77@gmail.com>
2024-11-22 14:51:45 -0800

    uroven

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        double a = 3.5;
+        double floor = Math.floor(a--);
+        double exp = Math.floor(a--);
+        double ert = Math.floor(a--);
+        System.out.println(a);
+
+
+
+}
+}
