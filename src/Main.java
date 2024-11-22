e6e8b26b131d8caaf7067359f75a71e4486f40da
malika099 <ergasevamadina77@gmail.com>
2024-11-22 15:27:36 -0800

    aaa

--- src/Main.java
+++ src/Main.java
@@ -97,7 +97,7 @@ public class Main {
         for (int i = 2; i <= 10; i++) {
             System.out.println("Таблица умножения для " + i + ":");
             for (int j = 1; j <= 10; j++) {
-                System.out.println(i + "*" + j + "=" + i * j);*/
+                System.out.println(i + "*" + j + "=" + i * j);
 
         int number = 567;
         int number1 = number%10;
@@ -110,6 +110,16 @@ public class Main {
         int num2 = num%10;
         int num3 = (num/10)%10;
         int num4 =(num/100)%10;
-        System.out.println(num4+ ","+num3+","+num2);
+        System.out.println(num4+ ","+num3+","+num2);*/
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = 0;
+        for (int i = a; i <= b; i++) {
+            if (i % 2 == 1) {
+                sum += i;
+            }
         }
     }
+}
