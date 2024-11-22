d724f2695793b5d42617cf86988a280db5d62d8b
malika099 <ergasevamadina77@gmail.com>
2024-11-22 15:01:39 -0800

    uroven2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        double a = 3.5;
+        /*double a = 3.5;
         double floor = Math.floor(a--);
         double exp = Math.floor(a--);
         double ert = Math.floor(a--);
-        System.out.println(a);
+        System.out.println(a);*/
+
+        int a = 321;
+        int b = a%10;
+        int c = (a/10)%10;
+        int e = (a/100)%10;
+        System.out.println(e+","+c+","+b);
 
 
 
-}
+
+    }
 }
