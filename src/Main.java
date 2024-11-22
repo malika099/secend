f595bed4a2a42e05bfe75f3655c8a7d7e0cb62e1
malika099 <ergasevamadina77@gmail.com>
2024-11-22 15:05:02 -0800

    uroven3

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,26 @@ public class Main {
         double floor = Math.floor(a--);
         double exp = Math.floor(a--);
         double ert = Math.floor(a--);
-        System.out.println(a);*/
+        System.out.println(a);
 
         int a = 321;
         int b = a%10;
         int c = (a/10)%10;
         int e = (a/100)%10;
-        System.out.println(e+","+c+","+b);
+        System.out.println(e+","+c+","+b);*/
 
+        int number = 567;
+        int number1 = number%10;
+        int number2  = (number/10)%10;
+        int number3 = (number/100)%10;
+        System.out.println(number3+ ","+number2+","+number1);
+
+
+        int num = 321;
+        int num2 = num%10;
+        int num3 = (num/10)%10;
+        int num4 =(num/100)%10;
+        System.out.println(num4+ ","+num3+","+num2);
 
 
 
